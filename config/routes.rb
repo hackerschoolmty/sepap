058d4176bc9ebb8718ea60819ad82d6928aa7db2
Abraham Kuri <kurenn@icalialabs.com>
2016-05-20 00:16:51 -0500

    Adds base api configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   resources :students
   resources :course_sessions
 
+   namespace :api, path: '/api' do
+    api_version(:module => "V1", :header => {:name => "Accept",
+                                             :value => "application/vnd.sepap.com+json; version=1"},
+                                             :defaults => {:format => :json}, :default => true) do
+    end
+  end
+
   root to: 'course_sessions#index'
 end
